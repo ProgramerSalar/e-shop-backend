aa1e4c3964fb421785ec6acd2616e928e99c0c01
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-12 13:40:15 +0530

    Create first Api call and Parsing json data

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ const express = require('express')  // express is module which is store in expre
 const app = express()
 
 
-app.get('/', (req, res) => {
+require('dotenv/config')   // access the dotenv file 
+const api = process.env.API_URL   // access the data in .env file 
+
+
+
+// http://localhost:3000/api/v1/products
+app.get(api+'/products', (req, res) => {     // slash is the parameter run the intital, take to request and response command 
     res.send("hello Api !")
 })
 
 
 app.listen(3000, 
     ()=>{
+        console.log(api)
         console.log("server is runnint  http://localhost:3000")  // this port have run the code 
     }
     
