ad74dc5294632e35fc1b438b24eb5acc9001ce26
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-14 16:51:07 +0530

    get category detail

--- routers/categories.js
+++ routers/categories.js
@@ -33,24 +33,37 @@ router.post('/', async (req, res)=> {
 })
 
 // api/vi/id(like this= 22223232323)
-router.delete('/:id', (req,res)=>{
-    Category.findByIdAndRemove(req.params.id)
-    .then(category => {
-        if (category){
-            return res.status(200).json({success:true, message:'the category is deleted'})
-        }
-        else{
-            return res.status(404).json({success:false, message:"category not found"})
-        }
-    }).catch((err)=>{
-        return res.status(400).json({success:false, error:err})
-    })
+// router.delete('/:id', (req,res)=>{
+//     Category.findByIdAndRemove(req.params.id)
+//     .then(category => {
+//         if (category){
+//             return res.status(200).json({success:true, message:'the category is deleted'})
+//         }
+//         else{
+//             return res.status(404).json({success:false, message:"category not found"})
+//         }
+//     }).catch((err)=>{
+//         return res.status(400).json({success:false, error:err})
+//     })
 
 
 
 
+// })
+
+// get the the category detail
+router.get('/:id', async(req,res)=>{
+    const category = await Category.findById(req.params.id)
+    if (!category){
+        res.status(500).json({
+            message:'The category with the given id is not found'
+        })
+    }
+    res.status(200).send(category)
 })
 
 
 
 module.exports = router
+
+
