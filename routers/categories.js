ba679e1f0039b4a9b5e74db7ce8675a3af3604d4
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-14 20:36:19 +0530

    Update Category

--- routers/categories.js
+++ routers/categories.js
@@ -33,34 +33,56 @@ router.post('/', async (req, res)=> {
 })
 
 // api/vi/id(like this= 22223232323)
-// router.delete('/:id', (req,res)=>{
-//     Category.findByIdAndRemove(req.params.id)
-//     .then(category => {
-//         if (category){
-//             return res.status(200).json({success:true, message:'the category is deleted'})
-//         }
-//         else{
-//             return res.status(404).json({success:false, message:"category not found"})
-//         }
-//     }).catch((err)=>{
-//         return res.status(400).json({success:false, error:err})
-//     })
+router.delete('/:id', (req,res)=>{
+    Category.findByIdAndRemove(req.params.id)
+    .then(category => {
+        if (category){
+            return res.status(200).json({success:true, message:'the category is deleted'})
+        }
+        else{
+            return res.status(404).json({success:false, message:"category not found"})
+        }
+    }).catch((err)=>{
+        return res.status(400).json({success:false, error:err})
+    })
 
 
 
 
-// })
+})
+
+
+
+router.put('/:id', async (req,res)=>{
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            name:req.body.name,
+            icon:req.body.icon,
+            color:req.body.color
+
+        },{
+            new:true
+        }
+    )
+    if(!category){
+        return res.status(400).send('the category cannot be created!')
+
+    }
+    
+    res.send(category)
+})
 
 // get the the category detail
-router.get('/:id', async(req,res)=>{
-    const category = await Category.findById(req.params.id)
-    if (!category){
-        res.status(500).json({
-            message:'The category with the given id is not found'
-        })
-    }
-    res.status(200).send(category)
-})
+// router.get('/:id', async(req,res)=>{
+//     const category = await Category.findById(req.params.id)
+//     if (!category){
+//         res.status(500).json({
+//             message:'The category with the given id is not found'
+//         })
+//     }
+//     res.status(200).send(category)
+// })
 
 
 
