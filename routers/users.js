553e37e7a9f89dad4e103724991bf6a40efd78d2
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-15 12:00:56 +0530

    Login User REST Api and crating to Token

--- routers/users.js
+++ routers/users.js
@@ -2,13 +2,18 @@ const express = require('express')
 const router = express.Router()
 const {User} = require('../models/user');
 let bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+
+
 
 
 router.post(`/`, async (req,res)=>{
+    const salt = bcrypt.genSaltSync(10);
+    const password = req.body.password
     let user = new User({
         name: req.body.name,
         email: req.body.email,
-        passwordHash: bcrypt.hashSync(req.body.passwordHash, 10),
+        passwordHash: bcrypt.hashSync(password, salt),
         phone: req.body.phone,
         isAdmin: req.body.isAdmin,
         street: req.body.street,
@@ -23,6 +28,57 @@ router.post(`/`, async (req,res)=>{
     return res.status(400).send('the user cannot be created!')
 
     res.send(user);
+})
+
+
+router.get(`/:id`, async(req,res)=>{
+    const user = await User.findById(req.params.id)
+    // const user = await User.find()
+
+    if(!user){
+        res.status(500).json({message:'Ther user with the given id is not match'})
+    }
+    res.status(200).send(user)
+})
+
+
+
+
+router.get(`/`, async(req,res)=>{
+    
+    const user = await User.find().select('-password')
+
+    if(!user){
+        res.status(500).json({message:'Ther user with the given id is not match'})
+    }
+    res.status(200).send(user)
+})
+
+
+
+router.post(`/login`, async(req, res)=>{
+    const user = await User.findOne({email:req.body.email})
+    const secret = process.env.secret
+    if(!user){
+        return res.status(400).send('The user not found')
+    }
+    if(user && bcrypt.compareSync(req.body.password, user.passwordHash)){
+        const token = jwt.sign(
+            {
+                userId:user.id
+            },
+            secret,
+            {
+                expiresIn:'1d'
+            }
+        )
+        // res.status(200).send('user Authenticated')
+        res.status(200).send({user:user.email, token:token})
+    }
+    else{
+        res.status(400).send('Password is wrong!')
+    }
+   
 })
 
 
