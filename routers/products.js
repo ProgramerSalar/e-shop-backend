e65e54b8307f38ecfe7d8dc6770f3017f112bbbb
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-16 11:20:27 +0530

    Product gallary multiple images uploaded

--- routers/products.js
+++ routers/products.js
@@ -261,4 +261,41 @@ router.get(``, async(req,res)=>{
     res.status(200).send(product)
 })
 
+
+
+router.put(
+    '/gallery-images/:id',
+    uploadOption.array('images', 10),
+    async (req, res) => {
+        if (!mongoose.isValidObjectId(req.params.id)) {
+            return res.status(400).send('Invalid Product Id');
+        }
+        const files = req.files;
+        let imagesPaths = [];
+        const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
+
+        if (files) {
+            files.map((file) => {
+                imagesPaths.push(`${basePath}${file.filename}`);
+            });
+        }
+
+        const product = await Product.findByIdAndUpdate(
+            req.params.id,
+            {
+                images: imagesPaths,
+            },
+            { new: true }
+        );
+
+        if (!product)
+            return res.status(500).send('the gallery cannot be updated!');
+
+        res.send(product);
+    }
+);
+
+
+
+
 module.exports = router
